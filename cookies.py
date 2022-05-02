f20e227896719fa73aea434969fcc03aa624aef0
thibauddvdm <35602123+thibauddvdm@users.noreply.github.com>
2022-05-02 16:21:32 +0200

    Update cookies.py

--- cookies.py
+++ cookies.py
@@ -5,12 +5,11 @@ from selenium.webdriver.chrome.service import Service
 
 SELENIUM_PORT = 9222
 
-def getCookies():
-    s = Service('C:\Selenium-Evidian-main\selenium\chromedriver.exe')
-    options = Options()
-    options.add_argument('user-data-dir')
-    driver = webdriver.Chrome(service=s)
-    driver.get("htpps://www.google.com")
-    pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+s = Service('C:\Selenium-Evidian-main\selenium\chromedriver.exe')
+options = Options()
+options.add_argument('user-data-dir')
+driver = webdriver.Chrome(service=s)
+driver.get("htpps://www.google.com")
+pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
 
-    driver.quit()
+driver.quit()
